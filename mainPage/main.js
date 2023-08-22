ce8cac928d82d13e1192caaec384fb9ee83b6d67
Na1x <na1x.1024@gmail.com>
2023-08-22 15:25:17 +0300

    feat: add functional accordion, finish about-section

--- mainPage/main.js
+++ mainPage/main.js
@@ -3,7 +3,7 @@ let burgerCheckboxLabel = document.querySelector(".burger-label");
 let burgerMenu = document.querySelector(".burger-menu");
 let burgerCheckboxButton = document.querySelector(".burger-button");
 let curtain = document.querySelector(".curtain");
-
+let accordionClickAreas = document.getElementsByClassName("about-accordion-list-part");
 
 //burger menu section start
 function switchBurgerMenu() {
@@ -29,19 +29,82 @@ function hideBurgerMenu() {
     burgerMenu.style.left = "100vw";
 }
 
-function darkerCurtain() {
-    curtain.style.opacity = "1";
-    curtain.style.zIndex = "9";
-}
+// function darkerCurtain() {
+//     curtain.style.opacity = "1";
+//     curtain.style.zIndex = "9";
+// }
 
-function brighterCurtain() {
-    curtain.style.opacity = "0";
-    curtain.style.zIndex = "-1";
-}
+// function brighterCurtain() {
+//     curtain.style.opacity = "0";
+//     curtain.style.zIndex = "-1";
+// }
+
 //burger menu end
 
-function main(){
+//accordion start
+
+function setClickToAccordionPart() {
+    for (let accordPart of accordionClickAreas) {
+        accordPart.addEventListener("mousedown", function () {
+
+    let accordPartContent = accordPart.querySelector('.about-accordion-content-container');
+            console.log(window.getComputedStyle(accordPartContent).height);
+            changeAccordionPartContent(accordPart);
+        })
+    }
+}
+
+function showAccordionPartContent(accordPart) {
+    let accordPartContent = accordPart.querySelector('.about-accordion-content-container');
+    let accordPartContentText = accordPart.querySelector('.about-accordion-text-content');
+    let accordPartContentImg = accordPart.querySelector('.about-accordion-image-content');
+    accordPartContent.style.maxHeight = '200px';
+    accordPartContentText.style.height = 'auto';
+    accordPartContentText.style.opacity = '1';
+    accordPartContentImg.style.maxHeight = '200px';
+    accordPartContentText.style.height = '200px';
+}
+
+function hideAccordionPartContent(accordPart) {
+    let accordPartContent = accordPart.querySelector('.about-accordion-content-container');
+    let accordPartContentText = accordPart.querySelector('.about-accordion-text-content');
+    let accordPartContentImg = accordPart.querySelector('.about-accordion-image-content');
+    accordPartContent.style.maxHeight = '0';
+    accordPartContentText.style.height = '0';
+    accordPartContentText.style.opacity = '0';
+    accordPartContentImg.style.maxHeight = '0';
+    accordPartContentText.style.height = '0';
+}
+
+function changeAccordionPartContent(accordPart) {
+    let accordPartContent = accordPart.querySelector('.about-accordion-content-container');
+    if(window.getComputedStyle(accordPartContent).height == '0px'){
+        showAccordionPartContent(accordPart);
+        showMinusIcon(accordPart);
+    }
+    else{
+        hideAccordionPartContent(accordPart);
+        showPlusIcon(accordPart);
+    }
+}
+
+function showPlusIcon(accordPart){
+    let accordPlusIcon = accordPart.querySelector('.about-accordion-heading-icon');
+    accordPlusIcon.style.clipPath = 'polygon(0 48%, 48% 48%, 48% 0, 52% 0, 52% 48%, 100% 48%, 100% 52%, 52% 52%, 52% 100%, 48% 100%, 48% 52%, 0 52%)';
+    accordPlusIcon.style.transform = 'rotate(0deg)';
+}
+
+function showMinusIcon(accordPart){
+let accordPlusIcon = accordPart.querySelector('.about-accordion-heading-icon');
+accordPlusIcon.style.clipPath = 'polygon(48% 0, 52% 0, 52% 100%, 48% 100%)';
+accordPlusIcon.style.transform = 'rotate(270deg)';
+}
+
+//accordion end
+
+function main() {
     switchBurgerMenu();
+    setClickToAccordionPart();
 }
 
 main();
